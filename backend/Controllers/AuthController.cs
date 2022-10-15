c2a1cb39c3797fc37454fced2dc932fdbe54d1cd
Marek Sokol <devmareksokol@gmail.com>
2022-10-15 23:52:55 +0200

    feat: register user

---
+++ backend/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Mvc;
+namespace backend.Controllers;
+using databaseContext;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class AuthController : ControllerBase
+{
+    private UserContext userContext = new UserContext();
+
+    [Route("login")]
+    [HttpPost]
+    public ActionResult<User> Post([FromBody] Login request)
+    {
+        var user = userContext.GetUserByEmail(request.Email);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        var passwordBytes = System.Text.Encoding.UTF8.GetBytes(request.Password);
+        var IsHashValid = AuthContext.IsHashValid(user.Password, request.Password);
+
+        if (!IsHashValid)
+        {
+            return NotFound();
+        }
+
+        return Ok(user);
+    }
+
+    [Route("register")]
+    [HttpPost]
+    public ActionResult<User> Post([FromBody] UserDTO request)
+    {
+        var userExists = userContext.GetUserByEmail(request.Email);
+        if (userExists != null)
+        {
+            return Conflict();
+        }
+        else
+        {
+            try
+            {
+                userContext.CreateUser(request, Role.User);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+            return Ok();
+        }
+    }
+}
---
+++ backend/model/UserDTO.cs
@@ -0,0 +1,40 @@
+using System.ComponentModel.DataAnnotations;
+
+public class UserDTO
+{
+    [Required]
+    [StringLength(100)]
+    public string Username { get; set; } = String.Empty;
+    [Required]
+    [StringLength(100)]
+    public string Password { get; set; } = String.Empty;
+    [Required]
+    [StringLength(100)]
+    [EmailAddress(ErrorMessage = "Invalid Email Address")]
+    public string Email { get; set; } = String.Empty;
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (Username.Length < 3 || Username.Length > 64)
+        {
+            yield return new ValidationResult(
+                "Username must be between 3 and 64 characters",
+                new[] { nameof(Username) }
+            );
+        }
+        if (Password.Length < 3 || Password.Length > 64)
+        {
+            yield return new ValidationResult(
+                "Password must be between 3 and 64 characters",
+                new[] { nameof(Password) }
+            );
+        }
+        if (Email.Length < 3 || Email.Length > 64)
+        {
+            yield return new ValidationResult(
+                "Email must be between 3 and 64 characters",
+                new[] { nameof(Email) }
+            );
+        }
+    }
+}
