b06221a62e5edf4155b36f5531f863dcb2324b5d
Marek Sokol <devmareksokol@gmail.com>
2022-12-18 01:54:14 +0100

    feat: page favicon

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,6 +3,6 @@ import Head from 'next/head'
 export const Header = () => (
     <Head>
         <title>NotePad</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
     </Head>
 )
